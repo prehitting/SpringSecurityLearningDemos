09b4edac71f17de8d1ef5f871b66193818ea35a3
PreparedCat <2480992876@qq.com>
2022-12-07 08:09:05 +0800

    feat: Authentication Architecture

--- security-module/src/main/java/com/prehitting/utils/IErrorCode.java
+++ security-module/src/main/java/com/prehitting/utils/IErrorCode.java
@@ -2,16 +2,20 @@ package com.prehitting.utils;
 
 /**
  * 常用API返回对象接口
- * Created by macro on 2019/4/19.
+ *
+ * @author macro
+ * @date 2019/4/19
  */
 public interface IErrorCode {
     /**
      * 返回码
+     * @return code
      */
     long getCode();
 
     /**
      * 返回信息
+     * @return message
      */
     String getMessage();
 }
